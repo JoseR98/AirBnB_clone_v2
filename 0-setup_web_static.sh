370a3176065f9c798545c546ae4b0609e78f4df0
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-17 20:46:55 +0000

    Fix errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-ufw allow "Nginx HTTP"
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow "Nginx HTTP"
 
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "<html>
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo bash -c 'echo "<html>
     <head>
     </head>
     <body>
-        Test Nginx configuration
+        Holberton School
     </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen [::]:80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+</html>" > /data/web_static/releases/test/index.html'
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
