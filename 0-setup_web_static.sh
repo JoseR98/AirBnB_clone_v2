e60ed70ac454ba563e72873d9421604b9ae0c7f2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-17 19:08:27 +0000

    Fix file paths

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static
-apt-get update
+apt-get -y update
 apt-get -y install nginx
 ufw allow "Nginx HTTP"
-mkdir -p data/web_static
-mkdir -p data/web_static/releases/test
-mkdir -p data/web_static/shared
+
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo "<html>
     <head>
     </head>
@@ -13,7 +13,7 @@ echo "<html>
         Test Nginx configuration
     </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/listen [::]:80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
