62ce93a544ce833a69d42cc639f11805268742e1
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-18 02:40:30 +0000

    Add comments

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,28 +21,41 @@ def do_deploy(archive_path):
 
         result = True
 
+        # Deploy compressed file to the server /tmp/ directory
         upload = put(archive_path, "/tmp/")
         if upload.failed:
             result = False
+
+        # Make dir to store the release
         dir_release = run("sudo mkdir -p " + curr_release)
         if dir_release.failed:
             result = False
+
+        # Uncompress file inside the folder created
         uncompress = run("sudo tar -xzf " + "/tmp/\
 " + file_ext + " -C " + curr_release)
         if uncompress.failed:
             result = False
+
+        # Move all files from web_static to folder release
         move_info = run("sudo mv " + curr_release + "\
 web_static/* " + curr_release)
         if move_info.failed:
             result = False
+
+        # Remove empty web_static directory
         rm_empty = run("sudo rm -rf " + curr_release + "\
 web_static/")
         if rm_empty.failed:
             result = False
+
+        # Remove symbolic link current
         rm_link = run("sudo rm -rf /data/\
 web_static/current")
         if rm_link.failed:
             result = False
+
+        # Make new symbolic link
         new_link = run("sudo ln -s " + curr_release + " /data/\
 web_static/current")
         if new_link.failed:
